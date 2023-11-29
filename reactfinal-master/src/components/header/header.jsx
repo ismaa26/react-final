daa0023013f40de8a2080b44fa41625252ea7f6a
ismaa26 <i.lozano@alumno.etec.um.edu.ar>
2023-11-29 00:59:26 -0300

    actualizacion

--- reactfinal-master/src/components/header/header.jsx
+++ reactfinal-master/src/components/header/header.jsx
@@ -7,7 +7,7 @@ export default function Header() {
             <nav className="head">
 
                 <div className="logo">
-                    <Link to="/home">Logo</Link>
+                    <Link to="/home">HOME</Link>
                 </div>
 
                 <div className="navbar">
@@ -31,7 +31,7 @@ export default function Header() {
                     <br />
                     <br />
                     <br />
-                    Somos un emprendimiento de ropa muy flama.
+                    SOMOS UN EMPRENDIMIENTO DE ROPA QUE SE ADAPTA A LAS NUEVAS MODAS
                 </p>
 
 
@@ -45,84 +45,16 @@ export default function Header() {
 
             </header>
             
-            <br/>
-
-            <section className="content sau">
-
-                <h2 className="title">Productos</h2>
-                <br/>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                    Possimus eligendi quod id sunt? Fuga,
-                    illum sequi facilis voluptatum odit libero expedita porro
-                    in corrupti ad architecto necessitatibus ex nemo illo!
-                </p>
-
-                <div className="box-container">
-                    <br/>
-
-                    <div className="box">
-
-                        <i className="fab fa-angular"></i>
-                        <h3>lorem</h3>
-                        <br/>
-                        <p>in corrupti ad architecto </p>
-
-                    </div>
-                    <br/>
-                    <div className="box">
-
-                        <i className="fab fa-apple"></i>
-                        <h3>lorem</h3>
-                        <br/>
-                        <p>in corrupti ad architecto </p>
-                        <br/>
-
-                    </div>
-                    
-                    <div className="box">
-
-                        <i className="fab fa-android"></i>
-                        <h3>lorem</h3>
-                        <br/>
-                        <p>in corrupti ad architecto </p>
-                        <br/>
-
-                    </div>   
-
-                </div>
-
-            </section>
 
             <section className="content about">
 
-                <h2 className="title">about</h2>
-                <br/>
-                <p>Lorem ipsum dolor sit amet consectetur 
-                    adipisicing elit. Quasi reprehenderit 
-                    placeat ullam voluptas itaque enim culpa 
-
-                </p>
-
-                <a href="#" className="btn">Saber Mas</a>
-
 
             </section>
-            <br/>
 
             <section className="content price">
 
-                <article className="contain">
-
-                    <h2 className="title">Precio</h2>
-                    <br />
-                    <p>El precio depende de que articulo de la pagina te gusta, para saberlo, visita: 
-                        <a href="#" className="btn">Articulos</a> 
-                    </p>
-                    <br />
-
-                </article>
             </section>
-            <br/><br/><br/><br/><br/>
+            
         </div>
     )
 }
--- reactfinal-master/src/components/producto/producto.jsx
+++ reactfinal-master/src/components/producto/producto.jsx
@@ -1,15 +1,24 @@
 import React from 'react';
-import vicera from '../../multimedia/gorra.png'
-
+import vicera from '../../multimedia/gorra.png';
 
 function Producto() {
+  // Define the pagar function
+  const pagar = () => {
+    // Implement the functionality you want when the button is clicked
+    console.log('Ir a Pagar clicked');
+    // You can add more logic here, like redirecting to a payment page or performing some other action
+  };
+
   return (
     <div className="producto">
       <img src={vicera} height="150px" width="20%" alt="gorrita" />
       <h3>Producto 1</h3>
       <p>Precio: $20.00</p>
       <p>Descripción: Este es un producto de ejemplo.</p>
-      <button className="boton4" onClick={pagar}>Ir a Pagar</button>
+      {/* Use the pagar function in the onClick handler */}
+      <button className="boton4" onClick={pagar}>
+        Ir a Pagar
+      </button>
     </div>
   );
 }
